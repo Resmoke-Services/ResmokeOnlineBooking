02ca6f233120ffaaa1ec7e87876decafde9aeb01
Resmoke Services <backendresmoke@gmail.com>
2025-09-08 15:16:37 +0000

    1. Primary Objective
    Prepare the entire Progressive Web App (PWA) proje

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,35 @@
 /** @type {import('next').NextConfig} */
-import withPWAInit from '@ducanh2912/next-pwa';
+import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  fallbacks: {
+    document: "/offline",
+  },
 });
 
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
-        pathname: '/v0/b/resmokeonlinebooking.firebasestorage.app/**',
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "/v0/b/resmokeonlinebooking.firebasestorage.app/**",
       },
     ],
   },
+  output: 'export',
 };
 
 export default withPWA(nextConfig);
