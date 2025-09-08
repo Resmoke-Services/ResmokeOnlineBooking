d1927a4b6fb3a8133e542ebd7b3545edb62e50be
Resmoke Services <backendresmoke@gmail.com>
2025-09-08 18:27:30 +0000

    I've identified the TypeScript error in src/app/booking/address-details/

--- src/app/item_to_repair/page.tsx
+++ src/app/item_to_repair/page.tsx
@@ -61,7 +61,7 @@ export default function ItemToRepairPage() {
     setItemsToRepair(finalItems);
     setProblemDescriptions(finalDescriptions);
     
-    router.push("/select_datetime");
+    router.push("/payment_and_terms");
   }
 
   if (!user) {
