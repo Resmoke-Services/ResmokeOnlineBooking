fac9924f908aaaa19734ea066df646e4b5d5b50f
Resmoke Services <backendresmoke@gmail.com>
2025-09-11 23:07:42 +0000

    images not loading

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,24 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+// Since __dirname is not available in ES modules, we create it
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingRoot: __dirname,
+  experimental: {
+    outputFileTracingRoot: __dirname,
+  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
       },
     ],
   },
