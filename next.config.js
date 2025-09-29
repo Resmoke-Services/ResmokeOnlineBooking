6d8060ec84b4d18e932a776fcdc8029b366ae75f
Resmoke Services <backendresmoke@gmail.com>
2025-09-29 18:46:40 +0000

    Updated 29/09/2025 20:47

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
-
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
@@ -45,4 +36,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
