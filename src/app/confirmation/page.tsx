9b11fe7c1560ea0acb6fb93d9f2d47a737728ab3
Resmoke Services <backendresmoke@gmail.com>
2025-10-04 15:49:58 +0000

    Error

--- src/app/confirmation/page.tsx
+++ src/app/confirmation/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import "dotenv/config";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useBookingStore } from "@/hooks/use-booking-store";
