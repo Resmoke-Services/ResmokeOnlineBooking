a3ed13a532b6d4205d63eb377408c02b77f09812
Resmoke Services <backendresmoke@gmail.com>
2025-10-04 12:41:27 +0000

    Why doesn't the webhook work: NEXT_PUBLIC_WEBHOOK_URL_AVAILABLE_TIME_SLO

--- src/app/api/webhooks/booking_confirmation/route.ts
+++ src/app/api/webhooks/booking_confirmation/route.ts
@@ -23,7 +23,7 @@ export async function POST(request: Request) {
       webhook: "booking_confirmation",
     };
 
-    const webhookResponse = await fetch(WEBHOOK_BASE_URL, {
+    const webhookResponse = await fetch(WEBHOOK_BASE-URL, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
