c3c2a2d727319789a55c43b12f47b94fc32f6e7c
Resmoke Services <backendresmoke@gmail.com>
2025-10-29 20:20:45 +0000

    starting build "d56ed4e1-74ca-46dd-8f0d-e5198e16d598" FETCHSOURCE From h

--- workspace/src/hooks/use-booking-store.ts
+++ workspace/src/hooks/use-booking-store.ts
@@ -26,7 +26,7 @@ interface BookingState extends BookingData {
   resetBooking: () => void;
 }
 
-const initialState: BookingState = {
+const initialState: Omit<BookingState, keyof ReturnType<typeof createStoreActions>> = {
   name: '',
   surname: '',
   cellNumber: '',
@@ -34,7 +34,7 @@ const initialState: BookingState = {
   addressDetails: {},
   formattedAddress: '',
   
-  bookingFor: 'personal', // Default value
+  bookingFor: 'PERSONAL',
   // Landlord
   landlordName: '',
   landlordSurname: '',
@@ -60,114 +60,94 @@ const initialState: BookingState = {
   servicePath: [],
   serviceType: null,
   availability: [],
-
-  // Actions are defined in the store creation, these are just placeholders for the type
-  setBookingFor: () => {},
-  setPersonalDetails: () => {},
-  setAddressDetails: () => {},
-  setLandlordDetails: () => {},
-  setOwnerDetails: () => {},
-  setCompanyDetails: () => {},
-  setItemsToRepair: () => {},
-  setProblemDescriptions: () => {},
-  setPaymentMethods: () => {},
-  setBillingInformation: () => {},
-  setTermsAgreement: () => {},
-  setSelectedDateTime: () => {},
-  setAvailability: () => {},
-  setWebhookConfirmation: () => {},
-  setServicePath: () => {},
-  setServiceType: () => {},
-  resetBooking: () => {},
 };
+
+const createStoreActions = (set: (updater: (state: BookingState) => Partial<BookingState> | Partial<BookingState>) => void) => ({
+  setBookingFor: (bookingFor: BookingFor) => set(() => ({ bookingFor })),
+  setPersonalDetails: (details: { name: string; surname: string; cellNumber: string; email: string }) => set(() => ({ ...details })),
+  setAddressDetails: (details: AddressDetails) => set(() => {
+    let parts: string[] = [];
+    const city = details.city === 'Other' ? details.otherCityDescription : details.city;
+    const suburb = details.suburb === 'Other' ? details.otherSuburb : details.suburb;
+
+    switch (details.propertyType) {
+        case 'Home':
+            if (details.houseNumber) parts.push(details.houseNumber);
+            if (details.streetName) parts.push(details.streetName);
+            break;
+        case 'Complex':
+            if (details.unitNumber) parts.push(`Unit ${details.unitNumber}`);
+            const complex = details.complexName === 'Other' ? details.otherComplexName : details.complexName;
+            if (complex) parts.push(complex);
+            if (details.streetNumber) parts.push(details.streetNumber);
+            if (details.streetName) parts.push(details.streetName);
+            break;
+        case 'House in an Estate':
+            if (details.standNumber) parts.push(`Stand ${details.standNumber}`);
+            if (details.houseNumber) parts.push(details.houseNumber);
+            if (details.streetNameInEstate) parts.push(details.streetNameInEstate);
+            if (details.estateName) parts.push(details.estateName);
+            break;
+        case 'Complex in an Estate':
+            if (details.unitNumber) parts.push(`Unit ${details.unitNumber}`);
+            const complexInEstate = details.complexName === 'Other' ? details.otherComplexName : details.complexName;
+            if (complexInEstate) parts.push(complexInEstate);
+            if (details.streetNameInEstate) parts.push(details.streetNameInEstate);
+            if (details.estateName) parts.push(details.estateName);
+            break;
+        case 'Office':
+            if (details.officeName) parts.push(details.officeName);
+            if (details.officeParkName) parts.push(details.officeParkName);
+            if (details.streetNumber) parts.push(details.streetNumber);
+            if (details.streetName) parts.push(details.streetName);
+            break;
+        case 'Small Holding':
+            if (details.holdingName) parts.push(details.holdingName);
+            if (details.streetName) parts.push(details.streetName);
+            break;
+        case 'Farm':
+            if (details.farmName) parts.push(details.farmName);
+            if (details.streetName) parts.push(details.streetName);
+            break;
+        case 'OTHER':
+            if (details.otherPropertyType) parts.push(details.otherPropertyType);
+            if (details.streetNumber) parts.push(details.streetNumber);
+            if (details.streetName) parts.push(details.streetName);
+            break;
+    }
+
+    if (suburb) parts.push(suburb);
+    if (city) parts.push(city);
+    
+    const formattedAddress = parts.filter(Boolean).join(', ');
+
+    return { 
+        addressDetails: details, 
+        formattedAddress
+    };
+  }),
+  setLandlordDetails: (details: { landlordName: string; landlordSurname: string; landlordCellNumber: string; landlordEmail: string; }) => set(() => ({ ...details })),
+  setOwnerDetails: (details: { ownerName: string; ownerSurname: string; ownerCellNumber: string; ownerEmail: string; }) => set(() => ({ ...details })),
+  setCompanyDetails: (details: { companyName: string; companyPhone: string; companyEmail: string; }) => set(() => ({ ...details })),
+  setItemsToRepair: (itemsToRepair: RepairItem[]) => set(() => ({ itemsToRepair })),
+  setProblemDescriptions: (problemDescriptions: Record<string, string>) => set(() => ({ problemDescriptions })),
+  setPaymentMethods: (paymentMethods: PaymentMethod[]) => set(() => ({ paymentMethods })),
+  setBillingInformation: (billingInformation: BillingInformation) => set(() => ({ billingInformation })),
+  setTermsAgreement: (termsAgreement: TermsAgreement | null) => set(() => ({ termsAgreement })),
+  setSelectedDateTime: (selectedDateTime: BookingSlot | null) => set(() => ({ selectedDateTime })),
+  setAvailability: (availability: AvailabilitySlot[]) => set(() => ({ availability })),
+  setWebhookConfirmation: (webhookConfirmation: WebhookConfirmation | null) => set(() => ({ webhookConfirmation })),
+  setServicePath: (servicePath: string[]) => set(() => ({ servicePath })),
+  setServiceType: (serviceType: ServiceType) => set(() => ({ serviceType })),
+  resetBooking: () => set(() => (initialState)),
+});
 
 
 export const useBookingStore = create<BookingState>()(
   persist(
     (set) => ({
       ...initialState,
-      setBookingFor: (bookingFor) => set({ bookingFor }),
-      setPersonalDetails: (details) => set({ ...details }),
-      setAddressDetails: (details: AddressDetails) => set((state) => {
-        let parts: string[] = [];
-        const city = details.city === 'Other' ? details.otherCityDescription : details.city;
-        const suburb = details.suburb === 'Other' ? details.otherSuburb : details.suburb;
-
-        switch (details.propertyType) {
-            case 'Home':
-                if (details.houseNumber) parts.push(details.houseNumber);
-                if (details.streetName) parts.push(details.streetName);
-                break;
-            case 'Complex':
-                if (details.unitNumber) parts.push(`Unit ${details.unitNumber}`);
-                const complex = details.complexName === 'Other' ? details.otherComplexName : details.complexName;
-                if (complex) parts.push(complex);
-                if (details.streetNumber) parts.push(details.streetNumber);
-                if (details.streetName) parts.push(details.streetName);
-                break;
-            case 'House in an Estate':
-                if (details.standNumber) parts.push(`Stand ${details.standNumber}`);
-                if (details.houseNumber) parts.push(details.houseNumber);
-                if (details.streetNameInEstate) parts.push(details.streetNameInEstate);
-                if (details.estateName) parts.push(details.estateName);
-                break;
-            case 'Complex in an Estate':
-                if (details.unitNumber) parts.push(`Unit ${details.unitNumber}`);
-                const complexInEstate = details.complexName === 'Other' ? details.otherComplexName : details.complexName;
-                if (complexInEstate) parts.push(complexInEstate);
-                if (details.streetNameInEstate) parts.push(details.streetNameInEstate);
-                if (details.estateName) parts.push(details.estateName);
-                break;
-            case 'Office':
-                if (details.officeName) parts.push(details.officeName);
-                if (details.officeParkName) parts.push(details.officeParkName);
-                if (details.streetNumber) parts.push(details.streetNumber);
-                if (details.streetName) parts.push(details.streetName);
-                break;
-            case 'Small Holding':
-                if (details.holdingName) parts.push(details.holdingName);
-                if (details.streetName) parts.push(details.streetName);
-                break;
-            case 'Farm':
-                if (details.farmName) parts.push(details.farmName);
-                if (details.streetName) parts.push(details.streetName);
-                break;
-            case 'OTHER':
-                if (details.otherPropertyType) parts.push(details.otherPropertyType);
-                if (details.streetNumber) parts.push(details.streetNumber);
-                if (details.streetName) parts.push(details.streetName);
-                break;
-        }
-
-        if (suburb) parts.push(suburb);
-        if (city) parts.push(city);
-        
-        const formattedAddress = parts.filter(Boolean).join(', ');
-        
-        const finalDetails = {
-            ...details,
-            accessCodeRequired: details.accessCodeRequired || 'no'
-        };
-
-        return { 
-            addressDetails: finalDetails, 
-            formattedAddress
-        };
-      }),
-      setLandlordDetails: (details) => set({ ...details }),
-      setOwnerDetails: (details) => set({ ...details }),
-      setCompanyDetails: (details) => set({ ...details }),
-      setItemsToRepair: (itemsToRepair) => set({ itemsToRepair }),
-      setProblemDescriptions: (problemDescriptions) => set({ problemDescriptions }),
-      setPaymentMethods: (paymentMethods) => set({ paymentMethods }),
-      setBillingInformation: (billingInformation) => set({ billingInformation }),
-      setTermsAgreement: (termsAgreement) => set({ termsAgreement }),
-      setSelectedDateTime: (selectedDateTime) => set({ selectedDateTime }),
-      setAvailability: (availability) => set({ availability }),
-      setWebhookConfirmation: (webhookConfirmation) => set({ webhookConfirmation }),
-      setServicePath: (servicePath) => set({ servicePath }),
-      setServiceType: (serviceType) => set({ serviceType }),
-      resetBooking: () => set(initialState),
+      ...createStoreActions(set),
     }),
     {
       name: 'resmoke-booking-storage-v5', 
